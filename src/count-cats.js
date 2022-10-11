363ab158dbb772558fefd117b5b567d0348974b7
VZalesinski <vzalesinski@gmail.com>
2022-10-11 15:07:39 +0300

    carbon-dating, count-cats, dream-team, what-season

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let cats = 0;
+  backyard.forEach((row) => {
+    row.forEach((elem) => {
+      if (elem === "^^") cats++;
+    });
+  });
+  return cats;
+  // let cats = 0;
+  // for (let i = 0; i < backyard.length; i++) {
+  //   for (let j = 0; j < backyard[i].length; j++) {
+  //     if (backyard[i][j] === "^^") cats++;
+  //   }
+  // }
+  // return cats;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,43 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!arr || !Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  if (arr.length === 0) return arr;
+  let transformedArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i] != typeof "number" ||
+      arr[i] !== "--double-next" ||
+      arr[i] !== "--discard-next" ||
+      arr[i] !== "--discard-prev" ||
+      arr[i] !== "--double-prev"
+    )
+      continue;
+    switch (arr[i]) {
+      case "--double-next":
+        transformedArr.push(arr[i + 1]);
+        transformedArr.push(arr[i + 1]);
+        break;
+      // case "--discard-next":
+      //   arr[i+1].
+    }
+  }
 }
 
 module.exports = {
-  transform
+  transform,
 };
