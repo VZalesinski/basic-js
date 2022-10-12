121a3086c374464b498e97a95f3e5dfa2a2c26a5
VZalesinski <vzalesinski@gmail.com>
2022-10-12 18:16:40 +0300

    finish transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -18,44 +18,36 @@ function transform(arr) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   if (arr.length === 0) return arr;
   let transformedArr = [];
+  let discardNext = false;
   for (let i = 0; i < arr.length; i++) {
-    if (
-      arr[i] != typeof "number" ||
-      arr[i] !== "--double-next" ||
-      arr[i] !== "--discard-next" ||
-      arr[i] !== "--discard-prev" ||
-      arr[i] !== "--double-prev"
-    )
-      return arr;
-
-    if (
-      arr[i - 1] === "--discard-next" ||
-      arr[i + 1] === "--discard-prev" ||
-      arr[0] === "--discard-prev" ||
-      arr[0] === "--double-prev"
-    )
+    if (discardNext) {
+      discardNext = false;
       continue;
+    }
 
     switch (arr[i]) {
       case "--double-next":
-        if (typeof arr[i + 1] == "number") {
-          transformedArr.push(arr[i + 1]);
-          break;
-        }
+        if (arr[arr.length - 1] === "--double-next") continue;
+        transformedArr.push(arr[i + 1]);
+        break;
 
       case "--double-prev":
-        if (arr[0] === "--double-prev") break;
-        if (typeof arr[i - 1] == "number") {
-          transformedArr.push(arr[i - 1]);
+        if (arr[0] === "--double-prev" || arr[i - 2] === "--discard-next")
+          continue;
+        transformedArr.push(arr[i - 1]);
+        break;
+      case "--discard-next":
+        discardNext = true;
+        break;
+      case "--discard-prev":
+        if (arr[0] === "--discard-prev" || arr[i - 2] === "--discard-next") {
+          continue;
+        } else {
+          transformedArr.pop();
           break;
         }
       default:
-        break;
-    }
-
-    if (typeof arr[i] == "number") {
-      transformedArr.push(arr[i]);
-      continue;
+        if (true) transformedArr.push(arr[i]);
     }
   }
   return transformedArr;
