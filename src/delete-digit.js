bd28286b3ae68b1888a04c9ae9a6e3bf4d7c40a3
VZalesinski <vzalesinski@gmail.com>
2022-10-12 18:54:00 +0300

    finish delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,25 +12,17 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function deleteDigit(n) {
-  // let collection = [];
-  // let arr = String(n).split("").map(Number);
-  // let sortArr = arr.slice().sort();
-  // let digit = sortArr.find((el) => el === 0);
-  // let deleteDigit = arr.indexOf(digit);
-  // arr.splice(deleteDigit, 0);
-  // return Number(arr.join(""));
-  // for (let i = 0; i < arr.length; i++) {
-  //   let curr = arr[i];
-  //   curr >= min ?
-  // if (curr === 0) {
-  //   arr.splice(arr.indexOf(curr), 0);
-  // } else {
-  //   collection.push(curr);
-  // }
-  // }
-  // return Number(collection.join(""));
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let arr = String(n);
+  let collection = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    collection.push(arr.slice(0, i) + arr.slice(i + 1));
+  }
+
+  return Math.max.apply(
+    null,
+    collection.map((el) => +el)
+  );
 }
 
 module.exports = {
